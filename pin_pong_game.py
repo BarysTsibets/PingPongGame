c62ddee4ecb70f37d65c9b30f3ada481a73e6051
molekyla08 <molekyla_08@mail.ru>
2020-06-29 18:43:38 -0700

    ping-pong update

--- pin_pong_game.py
+++ pin_pong_game.py
@@ -36,6 +36,23 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 0.5
+ball.dy = 0.5
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("red")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align = "center", font=("verdana", 22, "normal"))
+
+
+
+score_a = 0
+score_b = 0
 
 
 # Functions
@@ -76,6 +93,46 @@ win.onkeypress(racket_b_down, "Down")
 
 while True:
     win.update()
+
+    # Move Ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a,score_b), align = "center",
+                  font=("verdana", 22, "normal"))
+
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center",
+                  font=("verdana", 22, "normal"))
+
+
+
+    if ball.xcor() > 340 and ball.ycor() < racket_b.ycor() + 50 and ball.ycor() > racket_b.ycor() - 50:
+        ball.dx *= -1
+
+    if ball.xcor() < -340 and ball.ycor() < racket_a.ycor() + 50 and ball.ycor() > racket_a.ycor() - 50:
+        ball.dx *= -1
+
+
+
 
 
 
