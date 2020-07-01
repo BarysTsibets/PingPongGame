fefc29da6849ef87dff74cb97a8f0ca0563d2053
molekyla08 <molekyla_08@mail.ru>
2020-07-01 10:22:16 -0700

    v1

---
+++ list_scratch.py
@@ -0,0 +1,15 @@
+travel = ["cuba", "canada", "swiza", "germany", "italy"]
+# print(travel)
+#
+# print(sorted(travel))
+# print(sorted(travel,reverse=True))
+#
+# print(travel)
+# travel.reverse()
+print(travel)
+
+
+
+
+
+
